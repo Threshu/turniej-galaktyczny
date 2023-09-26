be8a9b4e8f21609c66bfc4063b330f49c866deae
Threshu <szymon.banach.dev@gmail.com>
2023-09-26 23:32:25 +0200

    implement vuechessboard

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,72 +5,55 @@
 
       <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
 
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+      <h1 class="text-h2 font-weight-bold">Illegal move</h1>
 
       <div class="py-14" />
-      <p>{{ $t("hello") }}</p>
+      <v-stepper :items="['Step 1', 'Step 2', 'Step 3']">
+  <template v-slot:item.1>
+    <v-card title="Step One" flat>1</v-card>
+  </template>
 
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
+  <template v-slot:item.2>
+    <v-card title="Step Two" flat>2</v-card>
+  </template>
 
-            Components
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
-          >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
-
-            Get Started
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
-
-            Community
-          </v-btn>
-        </v-col>
-      </v-row>
+  <template v-slot:item.3>
+    <v-card title="Step Three" flat>    <section>
+    <div>
+      <button @click="boardAPI?.toggleOrientation()">
+        Toggle orientation
+      </button>
+      <button @click="boardAPI?.resetBoard()">Reset</button>
+      <button @click="boardAPI?.undoLastMove()">Undo</button>
+      <button @click="boardAPI?.toggleMoves()">Threats</button>
+    </div>
+    <TheChessboard
+      :board-config="boardConfig"
+      @board-created="(api) => (boardAPI = api)"
+      @checkmate="handleCheckmate"
+    />
+  </section></v-card>
+  </template>
+</v-stepper>
     </v-responsive>
   </v-container>
 </template>
 
 <script setup lang="ts">
-  //
+import { TheChessboard } from 'vue3-chessboard';
+import 'vue3-chessboard/style.css';
+import type { BoardApi, BoardConfig } from 'vue3-chessboard';
+
+let boardAPI: BoardApi;
+const boardConfig: BoardConfig = {
+  coordinates: false,
+};
+
+function handleCheckmate(isMated: string) {
+  if (isMated === 'w') {
+    alert('Black wins!');
+  } else {
+    alert('White wins!');
+  }
+}
 </script>
