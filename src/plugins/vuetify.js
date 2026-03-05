8177411c1bca333e96108b70e0707b78a901a047
Threshu <szymon.banach.dev@gmail.com>
2026-03-05 13:51:42 +0100

    chore: update dependencies and refactor Vuetify plugin configuration
    - Updated firebase from version 8.0.1 to 8.10.1
    - Upgraded @vitejs/plugin-vue from version 4.0.0 to 5.2.0
    - Bumped vite from version 4.2.0 to 5.4.0
    - Updated vite-plugin-vuetify from version 1.0.0 to 2.1.0
    - Refactored vuetify.js to simplify component registration and maintain theme configuration

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,28 +1,18 @@
-import '@mdi/font/css/materialdesignicons.css';
-import 'vuetify/styles';
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
-import { createVuetify } from 'vuetify';
-import {
-	VDataTable,
-	VDataTableServer,
-	VDataTableVirtual,
-} from "vuetify/labs/VDataTable";
+import { createVuetify } from "vuetify";
 
 export default createVuetify({
-	components: {
-		VDataTable,
-		VDataTableServer,
-		VDataTableVirtual,
-	},
-	theme: {
-		themes: {
-			light: {
-				colors: {
-					primary: '#006420',
-					primaryLight: '#42b549',
-					secondary: '#42b549',
-				},
-			},
-		},
-	},
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: "#006420",
+          primaryLight: "#42b549",
+          secondary: "#42b549",
+        },
+      },
+    },
+  },
 });
